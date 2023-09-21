3197e5aa969d795eab599b0738ecf2748edc1b51
Indrajit Naiya <indrajitnaiya09@gmail.com>
2023-09-21 10:49:48 +0530

    broken test cases fixed -- in user model association added -- in enrollment model association added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.includes(:teacher_enrollments, :favorite_teachers)
   end
 
   # GET /users/1 or /users/1.json
@@ -58,13 +58,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :age, :kind)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :age, :kind)
+  end
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,7 @@ class Enrollment < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   belongs_to :teacher, foreign_key: :teacher_id, class_name: 'User'
   belongs_to :program
+  belongs_to :student, foreign_key: :user_id, class_name: 'User'
+
+  scope :favorites, -> { where(favorite:  true) }
 end
